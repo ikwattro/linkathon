189f1c1b1eceed55a8314c8ab2865498f28fabc4
Christophe Willemsen <willemsen.christophe@gmail.com>
2018-06-04 14:19:53 +0200

    fixed heroku config

--- bot.py
+++ bot.py
@@ -63,4 +63,5 @@ def handleSubjectQuery(alexaRequest):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
