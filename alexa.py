72ed11d6cb864291adf426f4b21e1fc7469379f3
Christophe Willemsen <willemsen.christophe@gmail.com>
2018-06-04 13:57:01 +0200

    initial commit

---
+++ alexa.py
@@ -0,0 +1,79 @@
+import json
+import os
+
+class AlexaRequest():
+
+    INTENT_REQUEST = "IntentRequest"
+    LAUNCH_REQUEST = "LaunchRequest"
+
+    def __init__(self, request):
+        self.sessionId = request['session']['sessionId']
+        self.userId = request['session']['user']['userId']
+        self.requestType = request['request']['type']
+        self.attributes = request['session']['attributes'] if 'attributes' in request['session'] else {}
+        req = request['request']
+        if 'intent' in req:
+            self.intent = req['intent']['name']
+            self.slots = req['intent']['slots'] if 'slots' in req['intent'] else []
+        else:
+            self.intent = ""
+        self.dialogState = req['dialogState'] if 'dialogState' in req else None
+    
+
+    def buildResponse(self, message=None, shouldEndSession=True, attributes=None, directives=None):
+        session_attributes = {} if attributes is None else attributes
+        should_end_session = shouldEndSession
+        card_title = "Brain Buddy"
+        reprompt_text = ""
+        speech_output = "Yo Bro! I am processing it." if message is None else message
+
+        return self.build_response(session_attributes, self.build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session, directives))
+    
+    def getIntent(self):
+        return self.intent
+    
+    def getSlots(self):
+        return self.slots
+    
+    def getAttributes(self):
+        return self.attributes
+    
+    def getRequestType(self):
+        return self.requestType
+    
+    def getDialogState(self):
+        return self.dialogState
+
+
+    def build_speechlet_response(self, title, outputText, reprompt_text, should_end_session, directives=None):
+        directivesValue = [] if directives is None else [{"type":directives}]
+        output = {}
+        output['cards'] = {
+                "type": "Simple",
+                "title": title,
+                "content": outputText
+            }
+        output['directives'] = directivesValue
+        if directives is None:
+            output['outputSpeech'] = {
+                "type": "PlainText",
+                "text": outputText
+            }
+            output['reprompt'] = {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": reprompt_text
+                }
+            }
+        output['shouldEndSession'] = should_end_session
+        return output
+
+    def build_response(self, session_attributes, speechlet_response):
+        return {
+            "version": "1.0",
+            "sessionAttributes": session_attributes,
+            "response": speechlet_response
+    }
+    
+
---
+++ bot.py
@@ -0,0 +1,59 @@
+import flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+import json
+import os
+from alexa import AlexaRequest
+import requests
+import time
+from neo4j.v1 import GraphDatabase
+
+app = flask.Flask(__name__)
+CORS(app,resources={r"/*": {"origins": "*"}})
+
+driver = GraphDatabase.driver(os.environ['NEO4J_URL'])
+session = driver.session()
+
+@app.route("/alexa", methods=['GET','POST'])
+def alexa():
+    data = request.get_json(force=True)
+    alexaRequest = AlexaRequest(data)
+
+    if alexaRequest.LAUNCH_REQUEST == alexaRequest.getRequestType():
+        return jsonify(alexaRequest.buildResponse('Welcome to Linkathon', False))
+    elif alexaRequest.getIntent() == 'subjectCount':
+        return jsonify(handleSubjectCount(alexaRequest))
+    elif alexaRequest.getIntent() == 'subjectQuery':
+        return jsonify(handleSubjectQuery(alexaRequest))
+    
+
+    return jsonify(alexaRequest.buildResponse())
+
+def handleSubjectCount(alexaRequest):
+    q = "MATCH (n:studySubject) RETURN count(n) AS c"
+    result = list(session.run(q))
+
+    return alexaRequest.buildResponse('There are ' + str(result[0]['c']) + ' subjects', True)
+
+def handleSubjectQuery(alexaRequest):
+    q = "MATCH (n:studySubject) WHERE n.%s %s toInteger(%s) RETURN count(n) AS c"
+    slots = alexaRequest.getSlots()
+    operator = slots['operator']['value']
+    operatorF = ''
+    if operator == 'greater than':
+        operatorF = '>'
+    else:
+        operatorF = '<'
+    
+    query = q % (slots['column']['value'], operatorF, slots['val']['value'])
+    print(query)
+    result = list(session.run(query))
+    c = result[0]['c']
+    
+    return alexaRequest.buildResponse('The operator is ' + operator + ' and there are ' + str(c) + ' matching subjects', True) 
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
